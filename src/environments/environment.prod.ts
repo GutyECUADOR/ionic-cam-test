1345f00736c155905f1fc7d88ccf44e90ca334f7
José L. Gutiérrez <gutiecuador@gmail.com>
2022-09-10 11:03:40 -0500

    fix ERR_CLEARTEXT_NOT_PERMITTED
    chrome://inspect
    para verificar problemas de http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://inversapp.com/'
+  API_URL: 'http://aulavirtualtics.com/backendinv/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8000/'
+  API_URL: 'http://aulavirtualtics.com/backendinv/'
 };
 
 /*
